ca5304ef9c598db7b43609c279725aafbdd3216b
Sofia Duarte <sofia.duarte@urosario.edu.co>
2023-05-26 21:42:23 -0500

    generos para todos los artistas, csv con todas las respuestas sanitizadas

--- algoritmos.py
+++ algoritmos.py
@@ -1,6 +1,8 @@
 import csv
 import networkx as nx
 import matplotlib.pyplot as plt
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 '''
 Graph Recommender: Clase que construye 2 tipos de grafos: La red entre los usuarios  y la red de artistas por géneros. (No es necesario hacer el grafo de cada usuario porque el algoritmo de recomendación solo itera por los artistas)
@@ -11,20 +13,49 @@ class Graph_Recommender():
 
     def __init__(self, doc) -> None:
         self.doc = doc
-        self.artistas = set() #Aquí se van a guardar un diccionario con llave el nombre del artista en mayúsculas, su código de Spotify y los géneros asociados a él
+        self.artistas = {} #Aquí se van a guardar un diccionario con llave el nombre del artista en mayúsculas, su código de Spotify y los géneros asociados a él
         self.usuarios = [] #Aquí se guardan los usuarios con su top 10 asociado a ellos en un diccionario
         self.grafo_usuarios = None
+
     def crear_usuarios(self):
         with open(self.doc) as d:
-            reader = csv.reader(d, delimiter=',')
+            reader = csv.reader(d, delimiter=';')
             for u in reader:
                 usuario = {}
                 for i,a in enumerate(u):
                     usuario[i+1] = a
-                    self.artistas.add(a)
+                    self.artistas[a]=[]
                 self.usuarios.append(usuario)
+
+    def get_artist_genres(self,artist_name):
+        # Set up Spotipy client credentials
+        client_id = '4417bca2fc0f405aa211b519e067bbaf'
+        client_secret = '9d53090efda44e968a6d4d5f5166ca36'
+        client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        # Search for the artist
+        results = sp.search(q=artist_name, type='artist', limit=1)
+        if 'artists' in results and 'items' in results['artists'] and len(results['artists']['items']) > 0:
+            artist = results['artists']['items'][0]
+            return artist['genres']
+        else:
+            return []
+        
     def fetch_genero_artistas(self):
-        pass
+        cont=0
+        cont2=0
+        for artista in self.artistas.keys():
+            
+            self.artistas[artista]=self.get_artist_genres(artista)
+        
+        #PARA VER ARTISTAS SIN GENERO
+        for artista in self.artistas.keys():
+            if self.artistas[artista]==[]:
+                print(artista, self.artistas[artista])
+                cont+=1
+            cont2+=1
+        print(cont, "/", cont2)
+
     def crear_grafo_usuarios(self):
         G = nx.Graph()
         #Crear los nodos numerados y con atributo el dict con el top de artistas
@@ -67,6 +98,7 @@ class Graph_Recommender():
         plt.show()
 
 
-g = Graph_Recommender('./test_respuestas.csv')
+g = Graph_Recommender('proyecto_grafos/respuestas.csv')
 g.crear_usuarios()
+g.fetch_genero_artistas()
 g.crear_grafo_usuarios()
